875945c23c901777015f47d768faf7da4b5f02a6
Alireza Soheili <71215827+alireza-constantin@users.noreply.github.com>
2022-12-02 14:23:59 +0330

    clean up day1

--- day1/a.ts
+++ day1/a.ts
@@ -1,11 +1,10 @@
 
 const input = await Deno.readTextFile('./input.txt')
 
-const arr = input.split('\r\n\r\n')
-const cals = arr.map(ar => (
-    ar.split('\r\n').reduce((prev, next) => +prev + +next, 0)
-))
-export const sorted = cals.toSorted((a, b) => b - a)
+export const sorted = input.split('\r\n\r\n')
+    .map(ar => (ar.split('\r\n')
+        .reduce((prev, next) => +prev + +next, 0)))
+    .toSorted((a, b) => b - a);
 
 const answer = sorted[0];
 console.log(answer)
