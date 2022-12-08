3867dc0ca8b1cf54fdbce0a0e66321e39cfb3d9d
Alireza Soheili <71215827+alireza-constantin@users.noreply.github.com>
2022-12-08 13:08:34 +0330

    day 7 done wtf aoc, wtf

--- day7/b.ts
+++ day7/b.ts
@@ -5,6 +5,12 @@ const stack: [[string, number]] = [['/', 0]]
 const MAX_AMOUNT = 100000;
 let total = 0;
 
+const TOTAL_SPACE = 70000000;
+const SPACE_FOR_UPDATE = 30000000;
+
+
+
+const arr: [[string, number]] = [['/', 0]]
 
 for (const line of text.filter(n => n)) {
     if (line == '$ cd /' || line == '$ ls') continue;
@@ -13,12 +19,12 @@ for (const line of text.filter(n => n)) {
     if (line.startsWith('$ cd')) {
         const dir = line[5]
         if (dir === '.') {
-            const [_, amount] = stack.pop()!
+            const [name, amount] = stack.pop()!
             if (amount <= MAX_AMOUNT) {
                 total += +amount
             }
             stack.at(-1)![1] += +amount
-
+            arr.push([name, amount])
         } else {
             stack.push([dir, 0])
         }
@@ -26,11 +32,25 @@ for (const line of text.filter(n => n)) {
     }
 
     const [amount, _] = line.split(' ')
-    // console.log('amount', amount)
     if (!isNaN(parseInt(amount))) {
         stack.at(-1)![1] += parseInt(amount);
     }
 
 }
 
-console.log(total)
+while (stack.length > 0) {
+    const [name, amount] = stack.pop()!;
+    arr.push([name, amount]);
+
+    if (stack.length > 0) {
+        stack.at(-1)![1] += amount;
+    }
+}
+
+const freeSpace = TOTAL_SPACE - +arr.at(-1)![1]
+const spaceToDel = SPACE_FOR_UPDATE - freeSpace
+
+const dirToDelete = arr.filter(([_, amount]) => amount >= spaceToDel).map(([_, amount]) => amount)
+
+const answer = Math.min(...dirToDelete)
+console.log(answer)
